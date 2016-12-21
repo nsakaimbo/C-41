6993efde3093b02d8ffcc204849b63ce6b4bea71
nsakaimbo <nsakaimbo@users.noreply.github.com>
2016-12-20 20:51:40 -0500

    [Swift] Adds devxoul Then Swift utility

---
+++ C-41/Then.swift
@@ -0,0 +1,62 @@
+//
+//  Then.swift
+//  C-41
+//
+//  Created by Nicholas Sakaimbo on 12/13/16.
+//  Copyright © 2016 Ash Furrow. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+public protocol Then {}
+
+extension Then where Self: Any {
+    
+    /// Makes it available to set properties with closures just after initializing and copying the value types.
+    ///
+    ///     let frame = CGRect().with {
+    ///       $0.origin.x = 10
+    ///       $0.size.width = 100
+    ///     }
+    public func with(_ block: (inout Self) -> Void) -> Self {
+        var copy = self
+        block(&copy)
+        return copy
+    }
+    
+    /// Makes it available to execute something with closures.
+    ///
+    ///     UserDefaults.standard.do {
+    ///       $0.set("devxoul", forKey: "username")
+    ///       $0.set("devxoul@gmail.com", forKey: "email")
+    ///       $0.synchronize()
+    ///     }
+    public func `do`(_ block: (Self) -> Void) {
+        block(self)
+    }
+    
+}
+
+extension Then where Self: AnyObject {
+    
+    /// Makes it available to set properties with closures just after initializing.
+    ///
+    ///     let label = UILabel().then {
+    ///       $0.textAlignment = .Center
+    ///       $0.textColor = UIColor.blackColor()
+    ///       $0.text = "Hello, World!"
+    ///     }
+    public func then(_ block: (Self) -> Void) -> Self {
+        block(self)
+        return self
+    }
+    
+}
+
+extension NSObject: Then {}
+
+extension CGPoint: Then {}
+extension CGRect: Then {}
+extension CGSize: Then {}
+extension CGVector: Then {}
