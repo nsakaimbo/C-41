146be3d956d12a1fb4384520f9644686cf310011
nsakaimbo <nsakaimbo@users.noreply.github.com>
2016-12-20 20:49:35 -0500

    [Swift] Update bridging header

--- C-41/C-41-Bridging-Header.h
+++ C-41/C-41-Bridging-Header.h
@@ -2,3 +2,18 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+// ReactiveCocoa
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
+// Cocoapods
+#import "RVMViewModel.h"
+
+// View Models
+#import <CoreData/CoreData.h>
+#import "ASHDetailViewModel.h"
+#import "ASHEditRecipeViewModel.h"
+#import "ASHEditStepViewModel.h"
+
+// Models
+#import "ASHRecipe.h"
+#import "ASHStep.h"
